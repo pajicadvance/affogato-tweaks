a8478de8e06c8209b1303f87f871a83afd2a00b6
pajicadvance <pavle.pajic23@gmail.com>
2024-02-15 14:18:27 +0100

    fix EMI showing vanilla tipped arrow recipe

---
+++ src/main/java/me/pajic/affogatotweaks/mixin/EmiVanillaPluginMixin.java
@@ -0,0 +1,17 @@
+package me.pajic.affogatotweaks.mixin;
+
+import dev.emi.emi.VanillaPlugin;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
+
+@Mixin(VanillaPlugin.class)
+public class EmiVanillaPluginMixin {
+
+    @ModifyArg(method = "lambda$register$12", at = @At(value = "INVOKE", target = "Lnet/minecraft/potion/PotionUtil;setPotion(Lnet/minecraft/item/ItemStack;Lnet/minecraft/potion/Potion;)Lnet/minecraft/item/ItemStack;", ordinal = 0))
+    private static ItemStack setEmiTippedArrowRecipe(ItemStack stack) {
+        return new ItemStack(Items.POTION);
+    }
+}
