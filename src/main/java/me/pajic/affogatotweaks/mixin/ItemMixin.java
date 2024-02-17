c80c103dcab5579367eca46996caed25cd8ae9df
pajicadvance <pavle.pajic23@gmail.com>
2024-02-17 22:14:03 +0100

    third time's the charm

--- src/main/java/me/pajic/affogatotweaks/mixin/ItemMixin.java
+++ src/main/java/me/pajic/affogatotweaks/mixin/ItemMixin.java
@@ -1,13 +1,11 @@
 package me.pajic.affogatotweaks.mixin;
 
 import de.siphalor.capsaicin.api.food.DynamicFoodPropertiesAccess;
-import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
-import net.minecraft.util.UseAction;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -16,11 +14,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(Item.class)
 public class ItemMixin {
-
-    @Inject(method = "getUseAction", at = @At("RETURN"), cancellable = true)
-    private void cancelUseIfFoodHungerRestorationIsZero1(ItemStack stack, CallbackInfoReturnable<UseAction> cir) {
-        cir.setReturnValue(stack.getItem().isFood() && DynamicFoodPropertiesAccess.create().withStack(stack).getModifiedFoodComponent().getHunger() != 0 ? UseAction.EAT : UseAction.NONE);
-    }
 
     @Inject(method = "use", at = @At("HEAD"), cancellable = true)
     private void cancelUseIfFoodHungerRestorationIsZero2(World world, PlayerEntity user, Hand hand, CallbackInfoReturnable<TypedActionResult<ItemStack>> cir) {
@@ -31,10 +24,5 @@ public class ItemMixin {
                 cir.cancel();
             }
         }
-    }
-
-    @Inject(method = "finishUsing", at = @At("RETURN"), cancellable = true)
-    private void cancelUseIfFoodHungerRestorationIsZero3(ItemStack stack, World world, LivingEntity user, CallbackInfoReturnable<ItemStack> cir) {
-        cir.setReturnValue(stack.getItem().isFood() && DynamicFoodPropertiesAccess.create().withStack(stack).getModifiedFoodComponent().getHunger() != 0 ? user.eatFood(world, stack) : stack);
     }
 }
