4dd664077084323c1f056490328dc2b9bc7da131
pajicadvance <pavle.pajic23@gmail.com>
2024-02-21 22:31:28 +0100

    nerf boat speed on ice

--- src/main/java/me/pajic/affogatotweaks/mixin/BlocksMixin.java
+++ src/main/java/me/pajic/affogatotweaks/mixin/BlocksMixin.java
@@ -4,7 +4,9 @@ import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(Blocks.class)
@@ -18,5 +20,15 @@ public class BlocksMixin {
     @Inject(method = "method_26136", at = @At("RETURN"), cancellable = true)
     private static void setWallTorchLightLevel(BlockState state, CallbackInfoReturnable<Integer> cir) {
         cir.setReturnValue(10);
+    }
+
+    @ModifyConstant(method = "<clinit>", constant = @Constant(floatValue = 0.98F))
+    private static float setIceSlipperiness(float constant) {
+        return 0.965F;
+    }
+
+    @ModifyConstant(method = "<clinit>", constant = @Constant(floatValue = 0.989F))
+    private static float setBlueIceSlipperiness(float constant) {
+        return 0.97F;
     }
 }
