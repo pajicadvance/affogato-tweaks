fead8e88cc9e0f0e35b3d796ad5418836bab6c6f
pajicadvance <pavle.pajic23@gmail.com>
2024-03-01 05:26:13 +0100

    nerf axe attack speed

--- src/main/java/me/pajic/affogatotweaks/mixin/ItemsMixin.java
+++ src/main/java/me/pajic/affogatotweaks/mixin/ItemsMixin.java
@@ -5,7 +5,9 @@ import net.minecraft.item.Items;
 import net.minecraft.util.Rarity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
 
 @Mixin(Items.class)
 public abstract class ItemsMixin {
@@ -39,5 +41,31 @@ public abstract class ItemsMixin {
     @ModifyArg(method = "<clinit>", at = @At(value = "INVOKE", target = "Lnet/minecraft/item/TridentItem;<init>(Lnet/minecraft/item/Item$Settings;)V"))
     private static Item.Settings setTridentDurability(Item.Settings settings) {
         return new Item.Settings().maxDamage(384);
+    }
+
+    // Changes the attack speed of axes
+    @ModifyConstant(method = "<clinit>", constant = @Constant(floatValue = -3.2F))
+    private static float setWoodenAndStoneAxeAttackSpeed(float constant) {
+        return -4.8F;
+    }
+
+    @ModifyConstant(method = "<clinit>", constant = @Constant(floatValue = -3.1F))
+    private static float setIronAxeAttackSpeed(float constant) {
+        return -4.8F;
+    }
+
+    @ModifyConstant(method = "<clinit>", constant = @Constant(floatValue = -3.0F, ordinal = 4))
+    private static float setGoldenAxeAttackSpeed(float constant) {
+        return -4.8F;
+    }
+
+    @ModifyConstant(method = "<clinit>", constant = @Constant(floatValue = -3.0F, ordinal = 8))
+    private static float setDiamondAxeAttackSpeed(float constant) {
+        return -4.8F;
+    }
+
+    @ModifyConstant(method = "<clinit>", constant = @Constant(floatValue = -3.0F, ordinal = 10))
+    private static float setNetheriteAxeAttackSpeed(float constant) {
+        return -4.8F;
     }
 }
