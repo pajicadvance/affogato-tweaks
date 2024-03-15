d751a1b4497862dda8693fca44aa8fe31e5b1551
pajicadvance <pavle.pajic23@gmail.com>
2024-03-15 23:19:08 +0100

    prevent using wool and carpets as fuel

--- src/main/java/me/pajic/affogatotweaks/mixin/AbstractFurnaceBlockEntityMixin.java
+++ src/main/java/me/pajic/affogatotweaks/mixin/AbstractFurnaceBlockEntityMixin.java
@@ -3,6 +3,7 @@ package me.pajic.affogatotweaks.mixin;
 import net.minecraft.block.entity.AbstractFurnaceBlockEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemConvertible;
+import net.minecraft.registry.tag.TagKey;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Constant;
@@ -23,5 +24,15 @@ public class AbstractFurnaceBlockEntityMixin {
     // Remove the dried kelp block from fuels
     @Redirect(method = "createFuelTimeMap", at = @At(value = "INVOKE", target = "Lnet/minecraft/block/entity/AbstractFurnaceBlockEntity;addFuel(Ljava/util/Map;Lnet/minecraft/item/ItemConvertible;I)V", ordinal = 27))
     private static void removeDriedKelpBlockFuel(Map<Item, Integer> fuelTimes, ItemConvertible item, int fuelTime) {
+    }
+
+    // Remove carpets from fuels
+    @Redirect(method = "createFuelTimeMap", at = @At(value = "INVOKE", target = "Lnet/minecraft/block/entity/AbstractFurnaceBlockEntity;addFuel(Ljava/util/Map;Lnet/minecraft/registry/tag/TagKey;I)V", ordinal = 17))
+    private static void removeCarpetFuel(Map<Item, Integer> fuelTimes, TagKey<Item> tag, int fuelTime) {
+    }
+
+    // Remove wool blocks from fuels
+    @Redirect(method = "createFuelTimeMap", at = @At(value = "INVOKE", target = "Lnet/minecraft/block/entity/AbstractFurnaceBlockEntity;addFuel(Ljava/util/Map;Lnet/minecraft/registry/tag/TagKey;I)V", ordinal = 14))
+    private static void removeWoolFuel(Map<Item, Integer> fuelTimes, TagKey<Item> tag, int fuelTime) {
     }
 }
