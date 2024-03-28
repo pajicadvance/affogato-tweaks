34217ec858d7d430b8dd0993c2956b12fef65e23
pajicadvance <pavle.pajic23@gmail.com>
2024-03-29 00:34:36 +0100

    reduce ravager roar knockback intensity significantly

---
+++ src/main/java/me/pajic/affogatotweaks/mixin/RavagerEntityMixin.java
@@ -0,0 +1,15 @@
+package me.pajic.affogatotweaks.mixin;
+
+import net.minecraft.entity.mob.RavagerEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
+
+@Mixin(RavagerEntity.class)
+public class RavagerEntityMixin {
+
+    @ModifyConstant(method = "knockBack", constant = @Constant(doubleValue = 0.001))
+    private double modifyRoarKnockbackMaxIntensity(double constant) {
+        return 0.1;
+    }
+}
