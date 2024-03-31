c761feddfa683caaacebd3b4087b035097340f74
pajicadvance <pavle.pajic23@gmail.com>
2024-03-31 01:11:10 +0100

    disable shulker duplication

---
+++ src/main/java/me/pajic/affogatotweaks/mixin/ShulkerEntityMixin.java
@@ -0,0 +1,16 @@
+package me.pajic.affogatotweaks.mixin;
+
+import net.minecraft.entity.mob.ShulkerEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(ShulkerEntity.class)
+public class ShulkerEntityMixin {
+
+    @Inject(method = "spawnNewShulker", at = @At("HEAD"), cancellable = true)
+    private void disableShulkerDuplication(CallbackInfo ci) {
+        ci.cancel();
+    }
+}
