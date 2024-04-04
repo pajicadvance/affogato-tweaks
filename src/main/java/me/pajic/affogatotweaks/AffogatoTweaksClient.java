88dd23a931b7bab554747785588d906b52864b06
pajicadvance <pavle.pajic23@gmail.com>
2024-04-04 16:19:35 +0200

    load item renames on the client only

--- src/main/java/me/pajic/affogatotweaks/AffogatoTweaksClient.java
+++ src/main/java/me/pajic/affogatotweaks/AffogatoTweaksClient.java
@@ -2,11 +2,20 @@ package me.pajic.affogatotweaks;
 
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
+import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
+import net.fabricmc.fabric.api.resource.ResourcePackActivationType;
+import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.client.render.RenderLayer;
+import net.minecraft.util.Identifier;
 
 public class AffogatoTweaksClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
+
+        FabricLoader.getInstance().getModContainer("affogatotweaks").ifPresent(modContainer ->
+                ResourceManagerHelper.registerBuiltinResourcePack(new Identifier("affogatotweaks","affogatotweaks_renames"),
+                        modContainer, ResourcePackActivationType.ALWAYS_ENABLED));
+
         BlockRenderLayerMap.INSTANCE.putBlock(AffogatoTweaks.COPPER_LANTERN, RenderLayer.getCutout());
         BlockRenderLayerMap.INSTANCE.putBlock(AffogatoTweaks.EXPOSED_COPPER_LANTERN, RenderLayer.getCutout());
         BlockRenderLayerMap.INSTANCE.putBlock(AffogatoTweaks.WEATHERED_COPPER_LANTERN, RenderLayer.getCutout());
