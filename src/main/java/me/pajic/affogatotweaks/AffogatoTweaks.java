8f5fa5af8d8e30186b1d41bdc08fb755bd55ffdc
pajicadvance <pavle.pajic23@gmail.com>
2024-04-10 19:09:28 +0200

    add waxed copper lanterns, make the light level falloff less harsh

--- src/main/java/me/pajic/affogatotweaks/AffogatoTweaks.java
+++ src/main/java/me/pajic/affogatotweaks/AffogatoTweaks.java
@@ -35,17 +35,29 @@ public class AffogatoTweaks implements ModInitializer {
         Registry.register(Registries.BLOCK, new Identifier("affogatotweaks", "exposed_copper_lantern"), EXPOSED_COPPER_LANTERN);
         Registry.register(Registries.BLOCK, new Identifier("affogatotweaks", "weathered_copper_lantern"), WEATHERED_COPPER_LANTERN);
         Registry.register(Registries.BLOCK, new Identifier("affogatotweaks", "oxidized_copper_lantern"), OXIDIZED_COPPER_LANTERN);
+        Registry.register(Registries.BLOCK, new Identifier("affogatotweaks", "waxed_copper_lantern"), WAXED_COPPER_LANTERN);
+        Registry.register(Registries.BLOCK, new Identifier("affogatotweaks", "waxed_exposed_copper_lantern"), WAXED_EXPOSED_COPPER_LANTERN);
+        Registry.register(Registries.BLOCK, new Identifier("affogatotweaks", "waxed_weathered_copper_lantern"), WAXED_WEATHERED_COPPER_LANTERN);
+        Registry.register(Registries.BLOCK, new Identifier("affogatotweaks", "waxed_oxidized_copper_lantern"), WAXED_OXIDIZED_COPPER_LANTERN);
 
         // Add oxidizable registry entries
         OxidizableBlocksRegistry.registerOxidizableBlockPair(COPPER_LANTERN, EXPOSED_COPPER_LANTERN);
         OxidizableBlocksRegistry.registerOxidizableBlockPair(EXPOSED_COPPER_LANTERN, WEATHERED_COPPER_LANTERN);
         OxidizableBlocksRegistry.registerOxidizableBlockPair(WEATHERED_COPPER_LANTERN, OXIDIZED_COPPER_LANTERN);
+        OxidizableBlocksRegistry.registerWaxableBlockPair(COPPER_LANTERN, WAXED_COPPER_LANTERN);
+        OxidizableBlocksRegistry.registerWaxableBlockPair(EXPOSED_COPPER_LANTERN, WAXED_EXPOSED_COPPER_LANTERN);
+        OxidizableBlocksRegistry.registerWaxableBlockPair(WEATHERED_COPPER_LANTERN, WAXED_WEATHERED_COPPER_LANTERN);
+        OxidizableBlocksRegistry.registerWaxableBlockPair(OXIDIZED_COPPER_LANTERN, WAXED_OXIDIZED_COPPER_LANTERN);
 
         // Register items
         Registry.register(Registries.ITEM, new Identifier("affogatotweaks", "copper_lantern"), new BlockItem(COPPER_LANTERN, new FabricItemSettings()));
         Registry.register(Registries.ITEM, new Identifier("affogatotweaks", "exposed_copper_lantern"), new BlockItem(EXPOSED_COPPER_LANTERN, new FabricItemSettings()));
         Registry.register(Registries.ITEM, new Identifier("affogatotweaks", "weathered_copper_lantern"), new BlockItem(WEATHERED_COPPER_LANTERN, new FabricItemSettings()));
         Registry.register(Registries.ITEM, new Identifier("affogatotweaks", "oxidized_copper_lantern"), new BlockItem(OXIDIZED_COPPER_LANTERN, new FabricItemSettings()));
+        Registry.register(Registries.ITEM, new Identifier("affogatotweaks", "waxed_copper_lantern"), new BlockItem(WAXED_COPPER_LANTERN, new FabricItemSettings()));
+        Registry.register(Registries.ITEM, new Identifier("affogatotweaks", "waxed_exposed_copper_lantern"), new BlockItem(WAXED_EXPOSED_COPPER_LANTERN, new FabricItemSettings()));
+        Registry.register(Registries.ITEM, new Identifier("affogatotweaks", "waxed_weathered_copper_lantern"), new BlockItem(WAXED_WEATHERED_COPPER_LANTERN, new FabricItemSettings()));
+        Registry.register(Registries.ITEM, new Identifier("affogatotweaks", "waxed_oxidized_copper_lantern"), new BlockItem(WAXED_OXIDIZED_COPPER_LANTERN, new FabricItemSettings()));
         Registry.register(Registries.ITEM, new Identifier("affogatotweaks", "copper_nugget"), COPPER_NUGGET);
         Registry.register(Registries.ITEM, new Identifier("affogatotweaks", "enchantment_upgrade"), ENCHANTMENT_UPGRADE_SMITHING_TEMPLATE);
 
@@ -57,6 +69,10 @@ public class AffogatoTweaks implements ModInitializer {
             content.addAfter(COPPER_LANTERN, EXPOSED_COPPER_LANTERN);
             content.addAfter(EXPOSED_COPPER_LANTERN, WEATHERED_COPPER_LANTERN);
             content.addAfter(WEATHERED_COPPER_LANTERN, OXIDIZED_COPPER_LANTERN);
+            content.addAfter(OXIDIZED_COPPER_LANTERN, WAXED_COPPER_LANTERN);
+            content.addAfter(WAXED_COPPER_LANTERN, WAXED_EXPOSED_COPPER_LANTERN);
+            content.addAfter(WAXED_EXPOSED_COPPER_LANTERN, WAXED_WEATHERED_COPPER_LANTERN);
+            content.addAfter(WAXED_WEATHERED_COPPER_LANTERN, WAXED_OXIDIZED_COPPER_LANTERN);
         });
 
         // Register Lavender guide book item
--- src/main/java/me/pajic/affogatotweaks/block/ModBlocks.java
+++ src/main/java/me/pajic/affogatotweaks/block/ModBlocks.java
@@ -2,6 +2,7 @@ package me.pajic.affogatotweaks.block;
 
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.Block;
+import net.minecraft.block.LanternBlock;
 import net.minecraft.block.MapColor;
 import net.minecraft.block.Oxidizable;
 import net.minecraft.block.piston.PistonBehavior;
@@ -9,22 +10,28 @@ import net.minecraft.sound.BlockSoundGroup;
 
 public class ModBlocks {
 
-    // Copper lantern variants
+    // Copper lanterns
     public static final Block COPPER_LANTERN = new CopperLanternBlock(
             Oxidizable.OxidationLevel.UNAFFECTED,
             FabricBlockSettings.create().mapColor(MapColor.ORANGE).solid().requiresTool().strength(3.0F).sounds(BlockSoundGroup.LANTERN).luminance(state -> 14).nonOpaque().pistonBehavior(PistonBehavior.DESTROY)
     );
     public static final Block EXPOSED_COPPER_LANTERN = new CopperLanternBlock(
             Oxidizable.OxidationLevel.EXPOSED,
-            FabricBlockSettings.create().mapColor(MapColor.TERRACOTTA_LIGHT_GRAY).solid().requiresTool().strength(3.0F).sounds(BlockSoundGroup.LANTERN).luminance(state -> 11).nonOpaque().pistonBehavior(PistonBehavior.DESTROY)
+            FabricBlockSettings.create().mapColor(MapColor.TERRACOTTA_LIGHT_GRAY).solid().requiresTool().strength(3.0F).sounds(BlockSoundGroup.LANTERN).luminance(state -> 12).nonOpaque().pistonBehavior(PistonBehavior.DESTROY)
     );
 
     public static final Block WEATHERED_COPPER_LANTERN = new CopperLanternBlock(
             Oxidizable.OxidationLevel.WEATHERED,
-            FabricBlockSettings.create().mapColor(MapColor.DARK_AQUA).solid().requiresTool().strength(3.0F).sounds(BlockSoundGroup.LANTERN).luminance(state -> 7).nonOpaque().pistonBehavior(PistonBehavior.DESTROY)
+            FabricBlockSettings.create().mapColor(MapColor.DARK_AQUA).solid().requiresTool().strength(3.0F).sounds(BlockSoundGroup.LANTERN).luminance(state -> 10).nonOpaque().pistonBehavior(PistonBehavior.DESTROY)
     );
     public static final Block OXIDIZED_COPPER_LANTERN = new CopperLanternBlock(
             Oxidizable.OxidationLevel.OXIDIZED,
-            FabricBlockSettings.create().mapColor(MapColor.TEAL).solid().requiresTool().strength(3.0F).sounds(BlockSoundGroup.LANTERN).luminance(state -> 3).nonOpaque().pistonBehavior(PistonBehavior.DESTROY)
+            FabricBlockSettings.create().mapColor(MapColor.TEAL).solid().requiresTool().strength(3.0F).sounds(BlockSoundGroup.LANTERN).luminance(state -> 8).nonOpaque().pistonBehavior(PistonBehavior.DESTROY)
     );
+
+    // Waxed variants
+    public static final Block WAXED_COPPER_LANTERN = new LanternBlock(FabricBlockSettings.copyOf(COPPER_LANTERN));
+    public static final Block WAXED_EXPOSED_COPPER_LANTERN = new LanternBlock(FabricBlockSettings.copyOf(EXPOSED_COPPER_LANTERN));
+    public static final Block WAXED_WEATHERED_COPPER_LANTERN = new LanternBlock(FabricBlockSettings.copyOf(WEATHERED_COPPER_LANTERN));
+    public static final Block WAXED_OXIDIZED_COPPER_LANTERN = new LanternBlock(FabricBlockSettings.copyOf(OXIDIZED_COPPER_LANTERN));
 }
