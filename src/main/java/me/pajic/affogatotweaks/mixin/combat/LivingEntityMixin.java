8237594458af2e763c381ada2b0de48f86ace171
pajic <32738446+pajicadvance@users.noreply.github.com>
2025-07-21 13:52:54 +0200

    Fix mob detection range

--- src/main/java/me/pajic/affogatotweaks/mixin/combat/LivingEntityMixin.java
+++ src/main/java/me/pajic/affogatotweaks/mixin/combat/LivingEntityMixin.java
@@ -1,29 +1,26 @@
 package me.pajic.affogatotweaks.mixin.combat;
 
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import com.llamalad7.mixinextras.sugar.Local;
-import com.llamalad7.mixinextras.sugar.ref.LocalDoubleRef;
 import net.minecraft.tags.DamageTypeTags;
 import net.minecraft.world.damagesource.DamageSource;
-import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(LivingEntity.class)
 public class LivingEntityMixin {
 
-    @Inject(
+    @ModifyExpressionValue(
             method = "getVisibilityPercent",
             at = @At(
-                    value = "INVOKE",
-                    target = "Lnet/minecraft/world/entity/LivingEntity;isDiscrete()Z"
+                    value = "CONSTANT",
+                    args = "doubleValue=0.8"
             )
     )
-    private void modifyVisibility(Entity lookingEntity, CallbackInfoReturnable<Double> cir, @Local LocalDoubleRef d) {
-        d.set((d.get() / 0.8) * 0.375);
+    private double modifyVisibilityWhenDiscrete(double original) {
+        return 0.375;
     }
 
     @ModifyArg(
