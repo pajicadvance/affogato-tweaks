b825cac00501c759b539a352fd217959b03e39df
pajic <32738446+pajicadvance@users.noreply.github.com>
2025-08-23 02:05:08 +0200

    Shaderlock changes

--- src/main/java/me/pajic/affogatotweaks/shaderlock/ShaderLock.java
+++ src/main/java/me/pajic/affogatotweaks/shaderlock/ShaderLock.java
@@ -5,6 +5,7 @@ import com.google.gson.GsonBuilder;
 import com.google.gson.JsonSyntaxException;
 import com.google.gson.reflect.TypeToken;
 import it.unimi.dsi.fastutil.objects.Object2ObjectArrayMap;
+import it.unimi.dsi.fastutil.objects.ObjectObjectImmutablePair;
 import me.pajic.affogatotweaks.Main;
 import net.fabricmc.loader.api.FabricLoader;
 import net.irisshaders.iris.Iris;
@@ -15,6 +16,9 @@ import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 import java.util.Map;
 import java.util.stream.Stream;
 
@@ -23,7 +27,7 @@ public class ShaderLock {
     private static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
     private static final Path FILE_PATH = FabricLoader.getInstance().getConfigDir().resolve("shaderlock");
     public static boolean setupMode = false;
-    public static final Map<String, Map<String, String>> SHADERS = new Object2ObjectArrayMap<>();
+    public static final Map<String, List<ObjectObjectImmutablePair<String, String>>> SHADERS = new Object2ObjectArrayMap<>();
 
     public static void init() throws IOException {
         try (Stream<Path> fileStream = Files.walk(Iris.getShaderpacksDirectory(), 1)) {
@@ -31,14 +35,14 @@ public class ShaderLock {
                 File shaderPack = shaderPackPath.toFile();
                 String shaderPackName = shaderPack.getName();
                 if (shaderPack.isDirectory() && !shaderPackName.equals("shaderpacks")) {
-                    Map<String, String> fileHashes = new Object2ObjectArrayMap<>();
+                    List<ObjectObjectImmutablePair<String, String>> fileHashes = new ArrayList<>();
                     try (Stream<Path> fileStream1 = Files.walk(shaderPackPath)) {
                         fileStream1.forEach(path -> {
                             try {
                                 File file = path.toFile();
                                 String fileName = file.getName();
                                 if (!fileName.startsWith(".") && !file.isDirectory()) {
-                                    fileHashes.put(SHA256.calculateSHA256(path.toFile()), fileName);
+                                    fileHashes.add(new ObjectObjectImmutablePair<>(SHA256.calculateSHA256(path.toFile()), fileName));
                                 }
                             } catch (IOException | NoSuchAlgorithmException e) {
                                 LOGGER.error("Failed to process shader file", e);
@@ -47,10 +51,11 @@ public class ShaderLock {
                     } catch (IOException e) {
                         LOGGER.error("Failed to process shader directory", e);
                     }
+                    fileHashes.sort(Comparator.comparing(ObjectObjectImmutablePair::right));
                     SHADERS.put(shaderPackName, fileHashes);
                 } else if (shaderPack.isFile() && shaderPackName.endsWith(".zip")) {
                     try {
-                        SHADERS.put(shaderPackName, Map.of(SHA256.calculateSHA256(shaderPack), shaderPackName));
+                        SHADERS.put(shaderPackName, List.of(new ObjectObjectImmutablePair<>(SHA256.calculateSHA256(shaderPack), shaderPackName)));
                     } catch (IOException | NoSuchAlgorithmException e) {
                         LOGGER.error("Failed to process shader archive", e);
                     }
@@ -67,7 +72,7 @@ public class ShaderLock {
                 fw.flush();
             }
         if (is != null && SHADERS.containsKey(name)) {
-            return SHADERS.get(name).equals(GSON.fromJson(new InputStreamReader(is), new TypeToken<Map<String, String>>() {}.getType()));
+            return SHADERS.get(name).equals(GSON.fromJson(new InputStreamReader(is), new TypeToken<List<ObjectObjectImmutablePair<String, String>>>() {}.getType()));
         }
         return false;
     }
