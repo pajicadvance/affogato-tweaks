0d231c9c5293d3a60fbda8502979f577649aaf6b
pajic <32738446+pajicadvance@users.noreply.github.com>
2025-08-23 02:19:22 +0200

    Disable ender dragon XP drop beyond first kill

---
+++ src/main/java/me/pajic/affogatotweaks/mixin/xp/EnderDragonMixin.java
@@ -0,0 +1,29 @@
+package me.pajic.affogatotweaks.mixin.xp;
+
+import com.llamalad7.mixinextras.injector.v2.WrapWithCondition;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.entity.boss.enderdragon.EnderDragon;
+import net.minecraft.world.level.dimension.end.EndDragonFight;
+import net.minecraft.world.phys.Vec3;
+import org.jetbrains.annotations.Nullable;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+
+@Mixin(EnderDragon.class)
+public class EnderDragonMixin {
+
+    @Shadow
+    private @Nullable EndDragonFight dragonFight;
+
+    @WrapWithCondition(
+            method = "tickDeath",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/world/entity/ExperienceOrb;award(Lnet/minecraft/server/level/ServerLevel;Lnet/minecraft/world/phys/Vec3;I)V"
+            )
+    )
+    private boolean noXpDropOnSubsequentKills(ServerLevel level, Vec3 pos, int amount) {
+        return dragonFight != null && !dragonFight.hasPreviouslyKilledDragon();
+    }
+}
