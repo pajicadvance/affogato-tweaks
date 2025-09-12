15bd8b0e3d7fabebf343314a4cea83ad73ad6d7e
pajic <32738446+pajicadvance@users.noreply.github.com>
2025-09-12 15:40:35 +0200

    Increase regular torch light level

--- src/main/java/me/pajic/affogatotweaks/values/MiscValues.java
+++ src/main/java/me/pajic/affogatotweaks/values/MiscValues.java
@@ -2,7 +2,7 @@ package me.pajic.affogatotweaks.values;
 
 public class MiscValues {
     public static final float EAT_TIME_INCREASE = 1.6F;
-    public static final int TORCH_LIGHT_LEVEL = 10;
+    public static final int TORCH_LIGHT_LEVEL = 12;
     public static final int COPPER_TORCH_LIGHT_LEVEL = 12;
     public static final int BLAZE_ROD_FUEL_TIME = 3200;
     public static final double SNEAK_RANGE_REDUCTION_MULT = 0.375;
